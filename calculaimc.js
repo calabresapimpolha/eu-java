d627aef5fe2d2e2d8f732d419256c715785a9c4d
calabresapimpolha <111508969+calabresapimpolha@users.noreply.github.com>
2022-10-06 10:36:08 -0300

    Create calculaimc.js

---
+++ calculaimc.js
@@ -0,0 +1,14 @@
+function validaPeso(peso) {
+    if (peso > 0 && peso < 1000) {
+        return true;
+    } else {
+        return false;
+    }
+}
+function validaAltura(altura) {
+    if (altura > 0 && altura <= 3.00) {
+        return true;
+    } else {
+        return false;
+    }
+}
